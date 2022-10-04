2b1281182d92d990e4a2560b9ed1ea52a5ea7891
Nicholas <91276870+NEOCLOUD0@users.noreply.github.com>
2022-10-04 10:36:06 -0400

    Add files via upload

---
+++ 6.Functions/Separate files/Source.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "threeintsfcts.h"
+#include "threeintsfcts.c"
+
+// Normally lots of other code here
+
+int main(void) {
+   
+   printf("%d\n", ThreeIntsSum(5, 10, 20));
+   printf("%d\n", ThreeIntsAvg(5, 10, 20));
+   
+   return 0;
+}
+
+// Normally lots of other code here
---
+++ 6.Functions/Separate files/threeintsfcts.h
@@ -0,0 +1,7 @@
+#ifndef FILENAME_H
+#define FILENAME_H
+
+int ThreeIntsSum(int num1, int num2, int num3);
+int ThreeIntsAvg(int num1, int num2, int num3);
+
+#endif
